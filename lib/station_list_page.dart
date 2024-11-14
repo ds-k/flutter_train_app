e275d3ed33ccb65072c08900acf80b51e4933552
Daeseong Kim <daeseongkim05@gmail.com>
2024-11-14 20:44:45 +0900

    feat: homePage에서 받은 리스트를 리스트뷰로 보여주고, pop할때 데이터를 보내주는 로직 추가

--- lib/station_list_page.dart
+++ lib/station_list_page.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 
 class StationListPage extends StatelessWidget {
-  const StationListPage({required this.title, super.key});
+  const StationListPage(
+      {required this.title, required this.station, super.key});
 
   final String title;
+  final List<String> station;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title), // 변경 필요
+        title: Text(title),
       ),
-      body: Column(
-        children: [
-          Container(
-            child: Text("수서"),
-          ),
-        ],
+      body: Container(
+        // decoration: BoxDecoration(
+        //     color: Color.fromRGBO(255, 246, 249, 1)), // appBar와 색상을 맞추기
+        child: ListView(
+            children: List.generate(station.length, (idx) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.pop(context, [station[idx], title]);
+            },
+            child: Container(
+              padding: EdgeInsets.only(left: 20),
+              decoration: BoxDecoration(
+                  border: Border(bottom: BorderSide(color: Colors.grey[300]!))),
+              width: double.infinity,
+              height: 50,
+              alignment: Alignment.centerLeft,
+              child: Text(
+                station[idx],
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+          );
+        })),
       ),
     );
   }
