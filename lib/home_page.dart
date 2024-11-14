1a2c160d4d0119b607c4e30b6d183a64056ce8a6
Daeseong Kim <daeseongkim05@gmail.com>
2024-11-14 15:24:22 +0900

    feat: 홈페이지 UI 그리기

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,19 +3,126 @@ import 'package:flutter_train_app/seat_page.dart';
 import 'package:flutter_train_app/station_list_page.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  String? departure;
+  String? arrival;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("test"),
-        ),
-        body: Column(
-          children: [SeatPage(), StationListPage()],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("기차 예매"),
+      ),
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return StationListPage();
+                              },
+                            ),
+                          );
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("출발역",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold)),
+                            Text(
+                              departure ?? "선택",
+                              style: TextStyle(fontSize: 40),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                        width: 2,
+                        height: 50,
+                        decoration: BoxDecoration(color: Colors.grey)),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return StationListPage();
+                              },
+                            ),
+                          );
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("도착역",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.grey,
+                                    fontWeight: FontWeight.bold)),
+                            Text(
+                              departure ?? "선택",
+                              style: TextStyle(fontSize: 40),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    onPressed: (() {}),
+                    child: Text(
+                      "좌석 선택",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+/*
+모서리 둥글기: 20
+버튼 색상: Colors.purple
+글자 색상: Colors.white
+글자 크기: 18
+글자 두께: FontWeight.bold
+*/
