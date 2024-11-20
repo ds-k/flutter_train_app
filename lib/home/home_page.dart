3c901bea22feac3f768462fef8df5247b86a1f6d
Daeseong Kim <daeseongkim05@gmail.com>
2024-11-20 17:22:59 +0900

    fix: bottomSheet의 widget 다듬기

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -253,12 +253,8 @@ class _HomePageState extends State<HomePage> {
                       height: 10,
                     ),
                     reservationList.isEmpty
-                        ? Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height * 0.4,
-                            alignment: Alignment.center,
+                        ? Expanded(
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Icon(
                                   Icons.error_outline,
